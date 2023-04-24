871c9ce623d4c548f8d98d6f1b30f62819ef3a05
Trimbak Pakhale <118504736+Trimbak15@users.noreply.github.com>
2023-04-24 12:23:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
-function getFormvalue() {
+function getFormvalue(a,b) {
     //Write your code here
+	const firstName = document.getElementById("first-name").value;
+  const lastName = document.getElementById("last-name").value;
 
+  alert("First Name: " + firstName + "\nLast Name: " + lastName);
 }
